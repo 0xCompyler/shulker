d38be395e70d1a33ac5a7599a4eeeeb74797f6a7
radioactive11 <roy.arijit2001@gmail.com>
2022-03-06 19:25:04 +0530

    :bookmark: `Stable` Transciption API working

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
 from app.api.v1.endpoints import touch
-
+from app.api.v1.endpoints import transcription
 
 api_router = APIRouter()
 
 api_router.include_router(touch.router, prefix="/touch")
+api_router.include_router(transcription.router, prefix="/transcribe")
